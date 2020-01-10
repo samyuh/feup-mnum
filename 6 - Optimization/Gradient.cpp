ff978e85953edad93809ee899280559d1cffcfd2
Samuel <diogosamuelfernandes@outlook.com>
2020-01-10 01:11:38 +0000

    Fixed Gradient

--- 6 - Optimization/Gradient.cpp
+++ 6 - Optimization/Gradient.cpp
@@ -11,19 +11,17 @@ double wy(double x, double y) { return -1.1*x +12; }
 void gradient(double x0, double y0, double lambda) {
     double  xn = x0, yn = y0;
     for (size_t i = 0; i < 10; i++) {
-        x0 = xn;
-        y0 = yn;
         cout << w(x0, y0) << endl;
         xn = x0 - lambda * wx(x0, y0);
         yn = y0 - lambda * wy(x0, y0);
 
-        if (w(xn, yn) < w(x0, y0))
+        if (w(xn, yn) < w(x0, y0)) {
             lambda *= 2;
-        else {
-            lambda /= 2;
-            xn = x0 - lambda * wx(x0, y0);
-            yn = y0 - lambda * wy(x0, y0);
+            x0 = xn;
+            y0 = yn;
         }
+        else
+            lambda /= 2;
     }
 }
 
